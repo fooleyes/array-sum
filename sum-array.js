e6dbfb120364bddd9d3a4677e8e70b0882e3732a
fooleyes <33706087+fooleyes@users.noreply.github.com>
2017-12-13 23:33:45 -0500

    Update sum-array.js

--- sum-array.js
+++ sum-array.js
@@ -1,18 +1,3 @@
-var ar = [1,2,3,4,10,11];
-
-var n = 6;
-
-function add(n, ar){
-	var sum = 0;
-	var i = 0;
-	while(i < n) {
-		sum += ar[i]
-		i++
-}
-       return sum;
-}
-
-// or for custum array
 
 function sumArray(arr){
 	var total = 0
